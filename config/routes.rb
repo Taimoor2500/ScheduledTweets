8d9147739fe118fb3cd16b4aa83b30abe924630d
Taimoor2500 <taimoor.rashid@dubizzlelabs.com>
2024-04-26 16:08:25 +0500

    initial

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+
+#root route
+#get"/", to:"main#index"
+root to: "main#index"
+
+  #Get /about
+  get "about-us", to:"about#index", as: :about
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
