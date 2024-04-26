e12978584594a44d688b277c6e42fdca61af3287
Taimoor2500 <taimoor.rashid@dubizzlelabs.com>
2024-04-26 16:36:09 +0500

    secod file

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController 
     def index
+        flash.now[:notice] = "Logged in successfully" #flash with .now persists 1 time 
+        flash.now[:alert] = "Invalid email or password"
     end
 end
